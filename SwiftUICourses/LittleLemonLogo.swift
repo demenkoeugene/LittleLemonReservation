3abcf880357157a5bd67716de6927a3c191c8b0f
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-05-18 15:12:26 +0300

    Project is Done

---
+++ SwiftUICourses/LittleLemonLogo.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonLogo.swift
+//  SwiftUICourses
+//
+//  Created by Eugene Demenko on 18.05.2023.
+//
+
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        Image("littlelemonlogo")
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
---
+++ SwiftUICourses/MainView.swift
@@ -0,0 +1,43 @@
+//
+//  MainView.swift
+//  SwiftUICourses
+//
+//  Created by Eugene Demenko on 18.05.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @StateObject var model = Model()
+    @State var tabSelection = 0
+    
+    var body: some View {
+        TabView (selection: $model.tabViewSelectedIndex){
+            LocationView() //4
+                .tag(0)
+                .tabItem {
+                    if !model.displayingReservationForm {
+                        Label("Locations", systemImage: "fork.knife")
+                    }
+                }
+            
+            ReservationView()
+                .tag(1)
+                .tabItem {
+                    if !model.displayingReservationForm {
+                        Label("Reservation", systemImage: "square.and.pencil")
+                    }
+                }
+        }
+        .environmentObject(model)
+
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
