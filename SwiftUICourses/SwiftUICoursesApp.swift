f31806dd4cc14591645e8e99660b4e0f646f79b6
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-05-18 12:42:48 +0300

    Initial Commit

---
+++ SwiftUICourses/SwiftUICoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoursesApp.swift
+//  SwiftUICourses
+//
+//  Created by Eugene Demenko on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
